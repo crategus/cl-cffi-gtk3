97120fe0e9070f0fde6fa9aaf0ec2e25aab14135
Crategus <kaiser@crategus.com>
2025-06-01 21:07:20 +0200

    Some changes to the example

--- example/pango-drawing.lisp
+++ example/pango-drawing.lisp
@@ -1,6 +1,6 @@
 ;;;; Drawing with Pango and Cairo
 ;;;;
-;;;; 2023-12-26
+;;;; 2025-06-01
 
 (in-package :gtk3-example)
 
@@ -14,13 +14,13 @@
                                  :default-width 400
                                  :default-height 400))
           (circle 100)
-          (n-words 12)
-          (font "Sans Bold 16"))
+          (words 12)
+          (font "Sans Bold 14"))
       (g:signal-connect window "destroy"
                         (lambda (widget)
                           (declare (ignore widget))
                           (gtk:leave-gtk-main)))
-      ;; Signals used to handle the backing surface
+      ;; Connect to "draw" signal to handle the backing surface
       (g:signal-connect area "draw"
          (lambda (widget cr)
            (let* ((cr (glib:pointer cr))
@@ -29,7 +29,7 @@
                   (radius (- (/ (min width height) 2) 20)))
              ;; Set up a transformation matrix so that the user space
              ;; coordinates for where we are drawing are [-RADIUS, RADIUS],
-             ;; [-RADIUS, RADIUS] We first center, then change the scale
+             ;; [-RADIUS, RADIUS]. We first center, then change the scale.
              (cairo:translate cr
                               (+ radius (/ (- width (* 2 radius)) 2))
                               (+ radius (/ (- height (* 2 radius)) 2)))
@@ -43,13 +43,13 @@
                   (desc (pango:font-description-from-string font)))
              (setf (pango:layout-text layout) "Text")
              (setf (pango:layout-font-description layout) desc)
-             ;; Draw the layout n-words times in a circle
+             ;; Draw the layout WORDS times in a circle
              (do* ((i 0 (+ i 1))
-                   (angle 0 (/ (* 360 i) n-words))
+                   (angle 0 (/ (* 360 i) words))
                    ;; Gradient from red to blue
                    (red (/ (+ 1 (cos (* (/ pi 180) (- angle 60)))) 2)
                         (/ (+ 1 (cos (* (/ pi 180) (- angle 60)))) 2)))
-                  ((>= i n-words))
+                  ((>= i words))
                (cairo:save cr)
                (cairo:set-source-rgb cr red 0 (- 1 red))
                (cairo:rotate cr (/ (* angle pi) 180))
